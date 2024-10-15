fee56800f6c512845eb2998029902d4eefc562f5
FilinMen <fiilinkadabra@tutamail.com>
2024-10-15 15:53:00 +0300

    at shcool

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-import math 
-
 input_data =open("input.txt","r")
 data =input_data.read()
 data = data.split()
 a = int(data[0])
 b = int(data[1])
-g = math.gcd(a,b) # для нод
-k = math.lcm(a,b) # для нок
+while a != 0 and b != 0: 
+        if a > b:
+            a = a % b
+            
+        else:
+            b = b % a
+            g = a + b
+n = (a * b) / g
+n = str(n)
 output_data = open("output.txt","w")
-output_data.write(k)
+output_data.write(n)
 input_data.close()
 output_data.close()
 
